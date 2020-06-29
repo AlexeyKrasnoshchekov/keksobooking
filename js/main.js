e257257984797cea8ca4206185661b958300f037
alexburnitdown <57058228+alexburnitdown@users.noreply.github.com>
2020-06-29 22:29:05 +0300

    Доработал модуль загрузки

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@
   var capacitySelectElement = document.querySelector('#capacity');
   var mainPin = document.querySelector('.map__pin--main');
 
+
+  window.load('https://up.htmlacademy.ru/assets/javascript/demo/8-xhr/unknownfile.json', window.load.onSuccess, window.load.onError);
+
+
   window.form.updateCurrentOfferLocation(window.pin.currentOfferLocation);
   window.map.disableMapFilters();
   window.form.disableAdForm();
