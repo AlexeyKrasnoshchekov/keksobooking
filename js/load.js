a26483d9665a9b2d0a6c2e721ca1cdaa32ab5c0c
Алексей <alexburnitdown@gmail.com>
2020-06-30 13:56:37 +0300

    Реализована загрузка данных с сервера 2

--- js/load.js
+++ js/load.js
@@ -1,18 +1,22 @@
 'use strict';
 
 (function () {
-  window.load = function (url, onSuccess, onError) {
-    var xhr = new XMLHttpRequest();
+  var url = 'https://javascript.pages.academy/keksobooking/data';
+  var StatusCode = {
+    OK: 200
+  };
+  var TIMEOUT_IN_MS = 10000;
 
+  window.load = function (onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
+      if (xhr.status === StatusCode.OK) {
         onSuccess(xhr.response);
       } else {
         onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
       }
-
     });
 
     xhr.addEventListener('error', function () {
@@ -23,7 +27,7 @@
       onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
     });
 
-    xhr.timeout = 1000;
+    xhr.timeout = TIMEOUT_IN_MS;
 
     xhr.open('GET', url);
     xhr.send();
