fde6337e8b3daebe1780f0003d4704096c8b74ab
alexburnitdown <57058228+alexburnitdown@users.noreply.github.com>
2020-07-27 19:39:08 +0300

    Последние изменения 5

--- js/formPhoto.js
+++ js/formPhoto.js
@@ -2,9 +2,16 @@
 
 (function () {
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  var ImgSize = {
+    AVATAR_WIDTH: 40,
+    AVATAR_HEIGHT: 44,
+    HOUSING_PHOTO_WIDTH: 70,
+    HOUSING_PHOTO_HEIGHT: 70
+  };
 
   var fileChooser = document.querySelector('input[type=file]');
-  var previewHeader = document.querySelector('.ad-form-header__preview img');
+  var avatarPreviewElement = document.querySelector('.ad-form-header__preview img');
+  var housingPhotoPreviewElement = document.querySelector('.ad-form__photo');
 
   fileChooser.addEventListener('change', function () {
     var file = fileChooser.files[0];
@@ -18,7 +25,15 @@
       var reader = new FileReader();
 
       reader.addEventListener('load', function () {
-        previewHeader.src = reader.result;
+        avatarPreviewElement.src = reader.result;
+        avatarPreviewElement.width = ImgSize.AVATAR_WIDTH;
+        avatarPreviewElement.height = ImgSize.AVATAR_HEIGHT;
+
+        var housingPhoto = document.createElement('img');
+        housingPhoto.src = reader.result;
+        housingPhoto.width = ImgSize.HOUSING_PHOTO_WIDTH;
+        housingPhoto.height = ImgSize.HOUSING_PHOTO_HEIGHT;
+        housingPhotoPreviewElement.appendChild(housingPhoto);
       });
 
       reader.readAsDataURL(file);
